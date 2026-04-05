956401d8532cb20a4c8b40ee6658ed9415858ac4
leliiaaa <146171208+leliiaaa@users.noreply.github.com>
2026-04-05 19:43:03 +0300

    Update main-sort.js

--- main-sort.js
+++ main-sort.js
@@ -5,11 +5,11 @@ function generateArray(size) {
 const baseArr = generateArray(100);
 
 console.log("%c 1. Нерозріджений масив (100 елементів) ", "background: #ff00ff; color: #fff");
-MySortLib.bubbleSort([...baseArr]);
-MySortLib.selectionSort([...baseArr]);
-MySortLib.insertionSort([...baseArr]);
-MySortLib.shellSort([...baseArr]);
-MySortLib.quickSort([...baseArr]);
+SortLib.bubbleSort([...baseArr]);
+SortLib.selectionSort([...baseArr]);
+SortLib.insertionSort([...baseArr]);
+SortLib.shellSort([...baseArr]);
+SortLib.quickSort([...baseArr]);
 
 console.log("%c 2. Розріджений масив (100 елементів) ", "background: #ff00ff; color: #ff69b4");
 
